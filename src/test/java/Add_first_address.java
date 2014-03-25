d95756fff9a9d478b2e59e47e744e45fb79fd5cd
RamiSaleem <rami.saleem@markavip.com>
2014-03-25 20:44:11 +0200

    Update Add_first_address.java

--- src/test/java/Add_first_address.java
+++ src/test/java/Add_first_address.java
@@ -174,17 +174,7 @@ public class Add_first_address implements SauceOnDemandSessionIdProvider, SauceO
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
-        
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-HashMap<String, Double> swipeObject = new HashMap<String, Double>();
-swipeObject.put("startX", 0.0);
-swipeObject.put("startY", 0.95);
-swipeObject.put("endX", 0.0);
-swipeObject.put("endY", 0.1);
-swipeObject.put("duration", 1.8);
-js.executeScript("mobile: swipe", swipeObject);
-
-
+  
         driver.findElement(By.xpath("//window[1]/navigationBar[1]/button[1]")).click();
         
            try {
